4ac575115e8b993c25db28dceb74f47a4580237f
Yong Bakos <ybakos@humanoriented.com>
2021-03-14 15:56:28 -0700

    Test: Demonstrate problems of associated tenants.
    This test associates a model from one tenant with a model from another
    tenant. We should not be able to save this model, but we can.

--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class PersonTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'prove it' do
+    ActsAsTenant.with_tenant(accounts(:one)) do
+      person = people(:one)
+      assert person.account == accounts(:one)
+      assert person.valid?
+      first_room = rooms(:one)
+      assert first_room.account == accounts(:one)
+      assert person.room == first_room
+
+      second_room = rooms(:two)
+      refute first_room.account == second_room.account
+      refute person.account == second_room.account
+      person.room = second_room # person's account is not the same as second_room's account
+
+      refute person.valid? # This should pass, but it fails.
+      refute person.save # This should pass, but it fails.
+      assert_raises(ActiveRecord::RecordInvalid) { person.save! } # This should pass, but it fails.
+    end
+  end
 end
