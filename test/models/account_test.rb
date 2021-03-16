5fc399d468159a9869bbbf2a736db8d6ebdfbc5d
Yong Bakos <ybakos@humanoriented.com>
2021-03-16 00:55:27 -0700

    Test: Demonsrate associations made with out-of-scope objects.
    This test mimics acts_as_tenant:/spec/models/model_extensions.rb:194,
    'associations can only be made with in-scope objects'.
    
    See https://github.com/ErwinM/acts_as_tenant/issues/255.

--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class AccountTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'associations can be made with out-of-scope objects (but should not!)' do
+    # See acts_as_tenant:/spec/models/model_extensions.rb:194,
+    # "associations can only be made with in-scope objects"
+    room2 = accounts(:two).rooms.create!(name: 'Account 2 room')
+    ActsAsTenant.current_tenant = accounts(:one)
+    room1 = Room.create!(name: "Account 1 room")
+    person = room1.people.create!(name: 'First account person')
+    person.room = room2
+    refute person.valid? # This should pass, but it fails!
+    refute person.update(room_id: room2.id) # This should pass, but it fails!
+  end
 end
